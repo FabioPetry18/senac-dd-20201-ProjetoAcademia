544abd0ebbfeec56d141d9e9d0c3bd9b62801744
Gustavo <Guh600@gmail.com>
2020-06-16 23:45:27 -0300

    Alterações modalidade

---
+++ src/main/java/controller/ModalidadeController.java
@@ -0,0 +1,11 @@
+package controller;
+
+public class ModalidadeController {
+
+	public String salvar(String nome, int alunos, String mensalidade) {
+		String mensagem = "";
+		
+		return mensagem;
+	}
+
+}
