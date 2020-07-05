95c989acbea5a21be9aaa8ec1c6e61a1d19c330e
Johnatan <Johnatan@10.0.1.3>
2020-07-04 21:25:35 -0300

    Alterações

--- src/main/java/view/PainelCadastroModalidade.java
+++ src/main/java/view/PainelCadastroModalidade.java
@@ -5,8 +5,11 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-
+import javax.swing.JFormattedTextField;
+import javax.swing.text.MaskFormatter;
 import controller.ModalidadeController;
+
+import java.text.ParseException;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
@@ -22,22 +25,26 @@ public class PainelCadastroModalidade extends JPanel {
 		setLayout(null);
 		
 		JLabel lblNome = new JLabel("Nome");
-		lblNome.setBounds(31, 34, 73, 14);
+		lblNome.setBounds(30, 30, 73, 14);
 		add(lblNome);
 		
 		txtNome = new JTextField();
-		txtNome.setBounds(150, 31, 238, 20);
+		txtNome.setBounds(30, 50, 238, 20);
 		add(txtNome);
-		txtNome.setColumns(10);
+		txtNome.setColumns(30);
 		
-		JLabel lblValorMensal = new JLabel("Mensalidade");
-		lblValorMensal.setBounds(31, 136, 73, 14);
-		add(lblValorMensal);
+		JLabel lblMensalidade = new JLabel("Valor mensal");
+		lblMensalidade.setBounds(30, 100, 150, 20);
+		add(lblMensalidade);
+		
+		JLabel lblReal = new JLabel("R$");
+		lblReal.setBounds(30, 120, 30, 20);
+		add(lblReal);
 		
 		txtMensalidade = new JTextField();
-		txtMensalidade.setBounds(150, 133, 238, 20);
+		txtMensalidade.setBounds(50, 120, 50, 20);
 		add(txtMensalidade);
-		txtMensalidade.setColumns(10);
+		txtMensalidade.setColumns(30);
 		
 		JButton btnCadastrar = new JButton("Cadastrar");
 		btnCadastrar.addActionListener(new ActionListener() {
